6989a3fdf8f0d02f820dcc11befd15cc0cd88ab6
Shivendra Narayan Tripathi <116185588+ShivendraNT@users.noreply.github.com>
2025-07-14 08:43:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.4 MB (33.97%)

---
+++ leetcode/0014-longest-common-prefix/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if len(strs)==0:
+            return  ''
+        prefix =strs[0]
+        for i in range(1,len(strs)):
+            while strs[i].find(prefix) !=0:
+                prefix = prefix[0:len(prefix)-1]
+                if prefix == '':
+                    return ''
+        return prefix
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        
