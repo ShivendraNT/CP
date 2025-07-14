dda8b40c71fc80455cee9fbd2d2288d45c74c2ff
Shivendra Narayan Tripathi <116185588+ShivendraNT@users.noreply.github.com>
2025-07-14 08:16:23 +0000

    [LeetCode Sync] Runtime - 40 ms (6.23%), Memory - 13.3 MB (79.47%)

---
+++ leetcode/0026-remove-duplicates-from-sorted-array/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        k=0
+        for i in nums:
+            if i not in nums[:k]:
+                nums[k]=i
+                k+=1
+        return k
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
