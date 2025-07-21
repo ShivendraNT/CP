494370916ff1c4e97e660e54e18e83282fdfab09
Shivendra Narayan Tripathi <116185588+ShivendraNT@users.noreply.github.com>
2025-07-21 12:20:21 +0000

    [LeetCode Sync] Runtime - 10 ms (59.78%), Memory - 12.5 MB (17.96%)

---
+++ leetcode/0009-palindrome-number/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        x=str(x)
+        y=x[::-1]
+        if x==y:
+            return True
+        else:
+            return False
+        """
+        :type x: int
+        :rtype: bool
+        """
+        
