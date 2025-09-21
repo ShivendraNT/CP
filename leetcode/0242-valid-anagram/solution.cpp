5493d0443481522495dea54b4c81e2d4613f22c8
Shivendra Narayan Tripathi <116185588+ShivendraNT@users.noreply.github.com>
2025-09-21 06:03:50 +0000

    [LeetCode Sync] Runtime - 8 ms (6.77%), Memory - 9.8 MB (49.95%)

--- leetcode/0242-valid-anagram/solution.cpp
+++ leetcode/0242-valid-anagram/solution.cpp
@@ -4,28 +4,11 @@ public:
         if (s.size()!=t.size()){
             return false;
         }
-        unordered_map<char,int> mp;
-        for (char i:s){
-            if (mp[i]){
-                mp[i]+=1;
-            }
-            else{
-                mp[i]=1;
-            }
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if (s==t){
+            return true;
         }
-        for (char i:t){
-            if (mp[i]){
-                if (mp[i]>0){
-                    mp[i]-=1;
-                }
-                else{
-                    return false;
-                }
-            }
-            else{
-                return false;
-            } 
-        }
-        return true;
+        return false;
     }
 };
