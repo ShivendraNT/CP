999c3b944e6555aea070544f726ccabd37d3d0ef
Shivendra Narayan Tripathi <116185588+ShivendraNT@users.noreply.github.com>
2025-10-25 16:55:56 +0000

    [LeetCode Sync] Runtime - 540 ms (33.96%), Memory - 243.5 MB (34.66%)

---
+++ leetcode/0493-reverse-pairs/solution.cpp
@@ -0,0 +1,50 @@
+int merge(vector<int> &nums,int low,int mid,int high){
+    vector<int> temp;
+    int cnt=0;
+    int left=low;
+    int right=mid+1;
+    int j = mid + 1;
+    for (int i = low; i <= mid; i++) {
+        while (j <= high && (long long)nums[i] > 2LL * nums[j]) j++;
+            cnt += (j - (mid + 1));
+        }
+    while(left<=mid&&right<=high){
+        if (nums[left]<=nums[right]){
+            temp.push_back(nums[left]);
+            left++;
+        }
+        else{
+            temp.push_back(nums[right]);
+            right++;
+        }
+    }
+    while(left<=mid){
+        temp.push_back(nums[left]);
+        left++;
+    }
+    while(right<=high){
+        temp.push_back(nums[right]);
+        right++;
+    }
+    for (int i=low;i<=high;i++){
+        nums[i]=temp[i-low];
+    }
+    return cnt;
+}
+int mergesort(vector<int> &nums,int low,int high){
+    if (low>=high) return 0;
+    int cnt=0;
+    int mid=(low+high)/2;
+    cnt+=mergesort(nums,low,mid);
+    cnt+=mergesort(nums,mid+1,high);
+    cnt+=merge(nums,low,mid,high);
+    return cnt;
+}
+
+class Solution {
+public:
+    int reversePairs(vector<int>& nums) {
+        int cnt=mergesort(nums,0,nums.size()-1);
+        return cnt;
+    }
+};
