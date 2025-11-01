1a4308d3eb0fed21822b17cb831dea2b533dd404
Shivendra Narayan Tripathi <116185588+ShivendraNT@users.noreply.github.com>
2025-11-01 16:03:46 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.5 MB (52.73%)

---
+++ leetcode/0162-find-peak-element/solution.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def findPeakElement(self, nums):
+        n=len(nums)
+        if (n==1):
+            return 0
+        if (nums[0]>nums[1]):
+            return 0
+        if (nums[n-1]>nums[n-2]):
+            return n-1
+        left=1
+        right=n-2
+        while(left<=right):
+            mid=left+(right-left)/2
+            if (nums[mid]>nums[mid-1] and nums[mid]>nums[mid+1]):
+                return mid
+            elif(nums[mid]>nums[mid-1]):
+                left=mid+1
+            else:
+                right=mid-1
+        return 0
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
