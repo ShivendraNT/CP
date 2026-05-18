b18832826085aef70a885f4bb595ad22ba2ec35b
Shivendra Narayan Tripathi <116185588+ShivendraNT@users.noreply.github.com>
2026-05-18 15:03:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.2 MB (82.39%)

---
+++ leetcode/1137-height-checker/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> original=heights;
+        sort(heights.begin(),heights.end());
+        int cnt=0;
+        for(int i=0;i<heights.size();i++){
+            if(heights[i]==original[i]) continue;
+            cnt++;
+        }
+        return cnt;
+    }
+};
