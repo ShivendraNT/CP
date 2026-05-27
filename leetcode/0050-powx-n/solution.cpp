9abe88168ae091963cc2cfb84fb321a209777364
Shivendra Narayan Tripathi <116185588+ShivendraNT@users.noreply.github.com>
2026-05-27 17:56:02 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.8 MB (13.18%)

--- leetcode/0050-powx-n/solution.cpp
+++ leetcode/0050-powx-n/solution.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
+    double binaryPow(double x, long long n) {
+        if (n == 0) return 1.0;
+        if (n % 2 == 0) return binaryPow(x * x, n / 2);
+        return x * binaryPow(x * x, n / 2);
+    }
+
     double myPow(double x, int n) {
-        long long N = n;   // use long long to handle overflow
+        long long N = n; 
         if (N < 0) {
             x = 1 / x;
             N = -N;
         }
-        return fastPow(x, N);
-    }
-
-private:
-    double fastPow(double x, long long n) {
-        if (n == 0) return 1.0;
-
-        double half = fastPow(x, n / 2);
-
-        if (n % 2 == 0)
-            return half * half;
-        else
-            return half * half * x;
+        return binaryPow(x, N);
     }
 };
-
-
