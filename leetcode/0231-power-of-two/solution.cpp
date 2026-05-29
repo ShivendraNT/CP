7f100bdb69bd88d92e8dcc89223ccedf44a48270
Shivendra Narayan Tripathi <116185588+ShivendraNT@users.noreply.github.com>
2026-05-29 18:18:17 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8 MB (12.55%)

--- leetcode/0231-power-of-two/solution.cpp
+++ leetcode/0231-power-of-two/solution.cpp
@@ -1,21 +1,9 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n==1){
-            return true;
-        }
-        if (n<0){
-            return false;
-        }
-        while (n>1){
-            if (n%2==1){
-                return false;
-            }
-            n=n/2;
-        }
-        if (n==0){
-            return false;
-        }
-        return true;
+        if(n==0) return false;
+        if(n==INT_MIN) return false;
+        if((n&n-1)==0) return true;
+        return false;
     }
 };
