3d78b637f5f3da6a7877bf24c03b3a1fe2f87003
Shivendra Narayan Tripathi <116185588+ShivendraNT@users.noreply.github.com>
2026-06-16 08:07:47 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.4 MB (14.62%)

---
+++ leetcode/3931-process-string-with-special-operations-i/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string ans="";
+        for(int i=0;i<s.size();i++){
+            if(s[i]>='a' && s[i]<='z'){
+                ans+=s[i];
+            }
+            else if(s[i]=='#'){
+                ans+=ans;
+            }
+            else if(s[i]=='%'){
+                reverse(ans.begin(),ans.end());
+            }
+            else{
+                ans=ans.substr(0,ans.size()-1);
+            }
+        }
+        return ans;
+    }
+};
