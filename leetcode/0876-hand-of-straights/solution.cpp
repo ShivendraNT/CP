fb5cb2d6ad37f67f950eaa70f459d83d91ff4e0a
Shivendra Narayan Tripathi <116185588+ShivendraNT@users.noreply.github.com>
2026-06-18 11:52:49 +0000

    [LeetCode Sync] Runtime - 39 ms (55.04%), Memory - 32.3 MB (63.19%)

---
+++ leetcode/0876-hand-of-straights/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int n=hand.size();
+        if(n%groupSize!=0) return false;
+
+        map<int,int> mp;
+        for(int i:hand){
+            mp[i]++;
+        }
+
+        while(!mp.empty()){
+            int curr=mp.begin()->first;
+
+            for(int i=0;i<groupSize;i++){
+                if(mp.find(curr+i)==mp.end()){
+                    return false;
+                }
+                mp[curr+i]--;
+                if(mp[curr+i]<1){
+                    mp.erase(curr+i);
+                }
+            }
+        }
+        return true;
+    }
+};
