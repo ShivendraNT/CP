9b41abfde32091ac3ce037b55abc8d84b1162d41
Shivendra Narayan Tripathi <116185588+ShivendraNT@users.noreply.github.com>
2026-06-20 14:28:49 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11 MB (74.48%)

---
+++ leetcode/0145-binary-tree-postorder-traversal/solution.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        TreeNode* curr=root;
+        stack<TreeNode*> st;
+        vector<int> ans;
+        while(curr || !st.empty()){
+            if(curr){
+                st.push(curr);
+                curr=curr->left;
+            }
+            else{
+                TreeNode* temp=st.top()->right;
+                if(!temp){
+                    temp=st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+                    while(!st.empty() && temp==st.top()->right){
+                        temp=st.top();
+                        st.pop();
+                        ans.push_back(temp->val);
+                    }
+                }
+                else{
+                    curr=temp;
+                }
+            }
+        }
+        return ans;
+    }
+};
