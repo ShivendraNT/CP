fc99d2f1f33e69ed7dd3053ca28e578edc906053
Shivendra Narayan Tripathi <shivendra.tripathi767@gmail.com>
2026-07-08 16:03:05 +0530

    Day zero

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CP
+# Shivendra Narayan Tripathi
+# Codeforces : [https://codeforces.com/profile/Shivendra77]
+# LeetCode : [https://leetcode.com/u/Shivendra77/]
+# Atcoder : [https://atcoder.jp/users/Shivendra77]
---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve() {
+    cout<<"Hello";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    cin >> t;   // remove if single test case
+    for (int i = 0; i < t; i++) solve();
+    return 0;
+}
