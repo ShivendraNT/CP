7b0d6cc6545d073789f613f2c5cb13f499bd9937
Shivendra Narayan Tripathi <116185588+ShivendraNT@users.noreply.github.com>
2026-07-25 12:18:59 +0000

    [LeetCode Sync] Runtime - 56 ms (66.59%), Memory - 185.9 MB (76.39%)

---
+++ leetcode/4211-count-subarrays-with-cost-less-than-or-equal-to-k/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, long long k) {
+        int n=nums.size();
+        deque<int> maxq,minq;
+        long long ans=0;
+        int l=0;
+        for(int r=0;r<n;r++){
+            while(!maxq.empty() && nums[maxq.back()] <= nums[r]) maxq.pop_back();
+            maxq.push_back(r);
+            while(!minq.empty() && nums[minq.back()] >= nums[r]) minq.pop_back();
+            minq.push_back(r);
+
+            while(l<=r && (long long)(nums[maxq.front()] - nums[minq.front()]) * (r - l + 1) > k){
+                if(maxq.front()==l) maxq.pop_front();
+                if(minq.front()==l) minq.pop_front();
+                l++;
+            }
+            ans += (r - l + 1);
+        }
+        return ans;
+
+    }
+};
