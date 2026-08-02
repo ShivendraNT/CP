570fc5737fca152d6ceaaf388391cad2624bc92a
Shivendra Narayan Tripathi <116185588+ShivendraNT@users.noreply.github.com>
2026-08-02 02:26:45 +0000

    [LeetCode Sync] Runtime - 3 ms (35.11%), Memory - 10.7 MB (35.85%)

---
+++ leetcode/0909-stone-game/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool predictTheWinner(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n, 0);
+        for(int i=n-1;i>=0;i--){
+            dp[i] = nums[i];
+            for(int j=i+1;j<n;j++){
+                int take_left=nums[i]-dp[j];
+                int take_right=nums[j]-dp[j - 1];
+                dp[j]=max(take_left,take_right);
+            }
+        }
+        return dp[n-1]>=0;
+    }
+    bool stoneGame(vector<int>& piles) {
+        return predictTheWinner(piles);
+    }
+};
