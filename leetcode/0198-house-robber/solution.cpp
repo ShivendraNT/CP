53636c03067a6e501d34c85b6fade8b026874e28
Shivendra Narayan Tripathi <116185588+ShivendraNT@users.noreply.github.com>
2026-08-16 16:43:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.8 MB (24.17%)

--- leetcode/0198-house-robber/solution.cpp
+++ leetcode/0198-house-robber/solution.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
+    /*
+    int f(vector<int>&nums,int house,int n,vector<int>&memo){
+        if(house>=n){
+            return 0;
+        }
+        if(memo[house]!=-1){
+            return memo[house];
+        }
+        // choose to rob this house
+        return max(nums[house]+f(nums,house+2,n,memo),f(nums,house+1,n,memo));
+    }
+    */
     int rob(vector<int>& nums) {
         int n=nums.size();
-        if(n==1){
-            return nums[0];
+        vector<int>dp(n+2);
+        dp[n+1]=0;
+        dp[n]=0;
+        for(int house=n-1;house>=0;house--){
+            dp[house]=max(nums[house]+dp[house+2],dp[house+1]);
         }
-        if(n==2){
-            return max(nums[0],nums[1]);
-        }
-        vector<int> dp(n,0);
-        int maxtheft=0;
-        dp[0]=nums[0];
-        dp[1]=nums[1];
-        for(int i=0;i<n;i++){
-            for(int j=i-2;j>=0;j--){
-                dp[i]=max(dp[i],nums[i]+dp[j]);
-            }
-            maxtheft=max(maxtheft,dp[i]);
-        }
-        return maxtheft;
+        return dp[0];
     }
 };
