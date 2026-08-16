ec292dad47e8010069fd533aeee6996ef7938d4a
Shivendra Narayan Tripathi <116185588+ShivendraNT@users.noreply.github.com>
2026-08-16 16:55:04 +0000

    [LeetCode Sync] Runtime - 19 ms (81.04%), Memory - 17.9 MB (84.32%)

--- leetcode/0322-coin-change/solution.cpp
+++ leetcode/0322-coin-change/solution.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
+    int INF=2e8;
+    /*
+    int f(vector<int>&coins,int amount,vector<int>&memo){
+        if(amount==0){
+            return 0;
+        }
+        else if(amount<0){
+            return INF;
+        }
+        if(memo[amount]!=-1){
+            return memo[amount];
+        }
+        int minreq=INF;
+        for(int i=0;i<coins.size();i++){
+            minreq=min(minreq,1+f(coins,amount-coins[i],memo));
+        }
+        return memo[amount]=minreq;
+    }
+    */
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, 1e9); 
-        
-        dp[0] = 0; 
-        
-        for (int coin : coins) {
-            for (int j = coin; j <= amount; j++) {
-                dp[j] = min(dp[j], dp[j - coin] + 1);
+        vector<int>dp(amount+1,INF);
+        dp[0]=0;
+        for(int i=0;i<=amount;i++){
+            for(int j=0;j<coins.size();j++){
+                if(i-coins[j]<0){
+                    continue;
+                }
+                dp[i]=min(dp[i],dp[i-coins[j]]+1);
             }
         }
-        
-        return (dp[amount] > amount) ? -1 : dp[amount];
+        return (dp[amount]==INF)?-1:dp[amount];
     }
 };
