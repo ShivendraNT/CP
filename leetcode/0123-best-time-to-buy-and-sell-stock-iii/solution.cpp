800e3e5a069e9cdefc8e83399dace82311e71d7c
Shivendra Narayan Tripathi <116185588+ShivendraNT@users.noreply.github.com>
2026-08-19 15:06:00 +0000

    [LeetCode Sync] Runtime - 463 ms (17.65%), Memory - 225.5 MB (9.76%)

--- leetcode/0123-best-time-to-buy-and-sell-stock-iii/solution.cpp
+++ leetcode/0123-best-time-to-buy-and-sell-stock-iii/solution.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
+    int f(int i,int holding,int tleft,vector<int>&prices,int n,vector<vector<vector<int>>>&memo){
+        if(i==n || tleft<=0){
+            return 0;
+        }
+        if(memo[i][holding][tleft]!=-1){
+            return memo[i][holding][tleft];
+        }
+        if(holding==0){
+            return memo[i][holding][tleft]=max(-prices[i]+f(i+1,1,tleft,prices,n,memo),f(i+1,0,tleft,prices,n,memo));
+        }
+        else{
+            return memo[i][holding][tleft]=max(prices[i]+f(i+1,0,tleft-1,prices,n,memo),f(i+1,1,tleft,prices,n,memo));
+        }
+        return -1;
+    }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
-        for(int ind=n-1;ind>=0;ind--){
-            for(int buy=0;buy<=1;buy++){
-                for(int cap=1;cap<=2;cap++){
-                    if(buy==1){
-                        dp[ind][buy][cap]=max(-prices[ind]+dp[ind+1][0][cap],dp[ind+1][1][cap]);
-                    }
-                    else{
-                        dp[ind][buy][cap]=max(prices[ind]+dp[ind+1][1][cap-1],dp[ind+1][0][cap]);
-                    }
-                }
-            }
-        }
-        return dp[0][1][2];
+        vector<vector<vector<int>>> memo(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
+        return f(0,0,2,prices,n,memo);
     }
 };
