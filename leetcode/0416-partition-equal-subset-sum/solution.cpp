2c88d6f51ccfc5a38dfbb15b64a38db7103466f4
Shivendra Narayan Tripathi <116185588+ShivendraNT@users.noreply.github.com>
2026-08-20 09:21:19 +0000

    [LeetCode Sync] Runtime - 216 ms (8.35%), Memory - 17.4 MB (66.78%)

--- leetcode/0416-partition-equal-subset-sum/solution.cpp
+++ leetcode/0416-partition-equal-subset-sum/solution.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& nums) {
-        int n = nums.size();
-        int totalSum = 0;
-        for(int x : nums) totalSum += x;
-        
-        if (totalSum % 2 != 0) return false;
-        int target = totalSum / 2;
-        
-        vector<bool> prev(target + 1, false);
-        
-        prev[0] = true;
-        
-        if (nums[0] <= target) prev[nums[0]] = true;
-        
-        for(int i = 1; i < n; i++) {
-            vector<bool> curr(target + 1, false);
-            curr[0] = true; 
-            
-            for(int j = 1; j <= target; j++) {
-                bool notTake = prev[j];
-                
-                bool take = false;
-                if(nums[i] <= j) {
-                    take = prev[j - nums[i]];
-                }
-                
-                curr[j] = take || notTake;
-            }
-            prev = curr;
+    /*
+    bool f(vector<int>&nums,int i,int sum,int currsum,vector<vector<int>>& memo,int n){
+        if(currsum==sum ){
+            return true;
         }
+        if(currsum>sum || i>=n){
+            return false;
+        }
+        if(memo[i][currsum]!=-1){
+            return (bool)memo[i][currsum];
+        }
+        bool take=f(nums,i+1,sum,currsum+nums[i],memo,n);
+        bool nottake=f(nums,i+1,sum,currsum,memo,n);
+        memo[i][currsum]=(int)(take||nottake);
+        return take||nottake;
+    }
+    */
+    bool canPartition(vector<int>& nums) {
+        int sum=0;
+        for(int i:nums){
+            sum+=i;
+        }
+        if(sum%2!=0){
+            return false;
+        }
+        int n=nums.size();
+        sum/=2;
         
-        return prev[target];
+        vector<vector<bool>> dp(n+1,vector<bool>(sum+1,false));
+        dp[n][0] = true;
+        for(int i=n-1;i>=0;i--){
+            for(int s=0;s<=sum;s++){
+                dp[i][s]=dp[i+1][s];
+                if(s >= nums[i]) dp[i][s] = dp[i][s] || dp[i+1][s - nums[i]];
+            }
+        }
+        return dp[0][sum];
     }
 };
